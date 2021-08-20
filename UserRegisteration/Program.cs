b85f0a8e68814353eb7c5e97f6b858e60cc6ab2a
Your Name <you@example.com>
2021-08-20 17:27:49 +0530

    feat:Enter Last Name and Check

--- UserRegisteration/Program.cs
+++ UserRegisteration/Program.cs
@@ -8,9 +8,12 @@ namespace UserRegisteration
         {
             Console.WriteLine("-----------------This is User Registration Problem--------------------");
             Registration user = new Registration();
-            Console.Write("Enter your first name : ");
+            Console.Write("Enter your first name:");
             string firstName = Console.ReadLine();
             user.Check(firstName);
+            Console.Write("Enter your last name:");
+            string lastName = Console.ReadLine();
+            user.Check(lastName);
         }
     }
 }
