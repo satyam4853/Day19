ab4122d75d48fb229bf57dcf2517d6422f0552a5
Your Name <you@example.com>
2021-08-20 17:20:48 +0530

    feat :Enter first valid name

--- UserRegisteration/Program.cs
+++ UserRegisteration/Program.cs
@@ -7,6 +7,10 @@ namespace UserRegisteration
         static void Main(string[] args)
         {
             Console.WriteLine("-----------------This is User Registration Problem--------------------");
+            Registration user = new Registration();
+            Console.Write("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
         }
     }
 }
---
+++ UserRegisteration/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegisteration
+{
+   public class Registration
+    {
+        public void Check(string name)
+        {
+            string pattern = "^[A-Z][a-z]{3}[a-z]*$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("Name {0} is Valid ", name);
+            }
+            else
+            {
+                Console.WriteLine("Name {0} is Invalid", name);
+            }
+        }
+
+
+    }
+}
